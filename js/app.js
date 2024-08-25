999f14affe5d220bf274bdb8497cf91740368be5
Umesh Kumar <166193048+umeshkumarofficial@users.noreply.github.com>
2024-08-25 15:33:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,34 @@
-$(document).ready(function(){
-$('.slider').slick({
-    arrows:false,
-    dots:true,
-    appendDots:'.slider-dots',
-    dotsClass:'dots'
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    appendDots: ".slider-dots",
+    dotsClass: "dots",
+  });
+
+  let hamberger = document.querySelector(".hamberger");
+  let times = document.querySelector(".times");
+  let mobileNav = document.querySelector(".mobile-nav");
+
+  hamberger.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+  });
+
+  times.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+  });
 });
 
-
-let hamberger = document.querySelector('.hamberger');
-let times = document.querySelector('.times');
-let mobileNav = document.querySelector('.mobile-nav');
-
-hamberger.addEventListener('click', function(){
-  mobileNav.classList.add('open');  
+document.getElementById("education-btn").addEventListener("click", function () {
+  document.getElementById("education-content").style.display = "block";
+  document.getElementById("about-content").style.display = "none";
+  this.classList.add("active");
+  document.getElementById("about-btn").classList.remove("active");
 });
 
-times.addEventListener('click', function(){
-    mobileNav.classList.remove('open');  
-});
-
+document.getElementById("about-btn").addEventListener("click", function () {
+  document.getElementById("education-content").style.display = "none";
+  document.getElementById("about-content").style.display = "block";
+  this.classList.add("active");
+  document.getElementById("education-btn").classList.remove("active");
 });
